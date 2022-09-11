20aa5e8efda105aafb9252f3b1c74bcd74250d02
Cindy Tu <cindyhfls@gmail.com>
2022-09-11 12:32:54 -0500

    fixed test_dvars and test_spm_funcs

--- findoutlie/tests/test_spm_funcs.py
+++ findoutlie/tests/test_spm_funcs.py
@@ -20,6 +20,7 @@ EXAMPLE_FILENAME = 'ds107_sub012_t1r2_small.nii'
 # Hint: sys.path
 # Hint: see the solutions if you are stuck.
 # +++your code here+++
+sys.path.append('/Users/jiaxintu/Documents/Python/nipraxis-work/diagnostics-NIL/findoutlie')
 
 import numpy as np
 
