1ab03ff2d3e14a50897313a8164b5eae24ace217
Andrew Van <vanandrew@wustl.edu>
2022-09-11 22:32:10 -0500

    :sparkles: implement dvars function

--- findoutlie/metrics.py
+++ findoutlie/metrics.py
@@ -3,6 +3,7 @@
 
 # Any imports you need
 # +++your code here+++
+import numpy as np
 
 
 def dvars(img):
@@ -21,12 +22,5 @@ def dvars(img):
         One-dimensional array with n-1 elements, where n is the number of
         volumes in `img`.
     """
-    # Hint: remember 'axis='.  For example:
-    # In [2]: arr = np.array([[2, 3, 4], [5, 6, 7]])
-    # In [3]: np.mean(arr, axis=1)
-    # Out[2]: array([3., 6.])
-    #
-    # You may be be able to solve this in four lines, without a loop.
-    # But solve it any way you can.
-    # This is a placeholder, replace it to write your solution.
-    raise NotImplementedError('Code up this function')
+    # compute dvars and return
+    return (np.diff(img.get_fdata(), axis=3) ** 2).mean(axis=(0, 1, 2)) ** 0.5
